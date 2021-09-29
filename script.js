2fe08336f942fbb9a38c35cf9261c070e83fa07f
Payab <91420578+Payab@users.noreply.github.com>
2021-09-29 17:15:48 +0200

    If command
    Added If command for changes screen color to pink

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ button.addEventListener("click", function(){
     console.log("Hello world")
 })
 
+Let knappa = true;
+const lightBtn = document.querySelector (".light-Btn");
+lightBtn.addEventListener ("Click", function (){
+    if (knapp === tru){
+        document.body.dtyle.backgroundColor = "Pink"
+        knapp = false;
+    }
+    else{
+        document.body.style.backgroundColor = "White"
+        knappa = true;
+    }
+})
